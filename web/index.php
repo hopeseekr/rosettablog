50d8b4bf06b7aee30fe6b0db4c157af42a980c69
hopeseekr <hopeseekr@xmule.ws>
2010-08-08 21:10:00 -0500

    Wrote intro to the Front Controller.

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Single Page MVC
+* MVC stands for the Model View Controller Design Pattern.
+* 
+* This is a very popular design pattern for websites because it 
+*    * allows for far less cluttered and more organized code
+*    * makes it far more easier to share code between pages and to add new pages
+*    * separates XHTML, CSS and JavaScript (the Views), allowing for
+*      XHTML editing by persons unfamiliar with PHP (important for businesses)
+*    * separates business logic (Model) from the View
+*    * acts as a defacto standard for organizing a site that professionals readily comprehend.
+*/
+
