b17dd5d50ebb58cccc936693df6c0d6342f08878
hopeseekr <hopeseekr@xmule.ws>
2010-08-08 22:02:37 -0500

    ViewController: 1. Move isValidView() into the ViewController.

--- web/index.php
+++ web/index.php
@@ -13,44 +13,50 @@
 */
 
 /* This file, index.php, will be our Front-Controller.  This file's responsibility will be to
-   make the MVC work by acting as the front glue that ties the various components together. */
+   make the MVC work by acting as the front glue that ties the various components together.
    
-/* Every page in our web app will be determined based on what view it is calling and what action
+   Every page in our web app will be determined based on what view it is calling and what action
    it is performing.  Let's call our default view "home" and our default action "index".
-   
-   1. Now, let's add the code necessary to find out what view and action we will display. */
+*/   
 
-// This means, "If the URL parameter view= is set, grab its sanitized value via the input filter, or 
-// use the default value 'home'."
+// Front Controller: 1. Find out what view and action we will display.
+
+// The code below means, "If the URL parameter view= is set, grab its sanitized value via the input
+// filter, or use the default value 'home'."
 $view = isset($_GET['view']) ? filter_input(INPUT_GET, 'view', FILTER_SANITIZE_STRING) : 'home';
 $action = isset($_GET['action']) ? filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) : 'index';
 
-// 2. Let's create a function to determine what our available views are.
-function isValidView($view)
+// ViewController: 1. Move isValidView() into the ViewController.
+class ViewController
 {
+	// 1a. Let's rewrite the constants as class constants.
 	// ProTip: Use constants to define things like status codes to make your source code far easier
 	//         to understand.  Per the PHP standard, return true on success.
-	define('VALID_VIEW', true);
-	define('ERROR_INVALID_FILE_NAME', 1);
-	define('ERROR_FILE_NOT_FOUND', 2);
+	const VALID_VIEW = true;
+	const ERROR_INVALID_FILE_NAME = 1;
+	const ERROR_FILE_NOT_FOUND = 2;
 
-	// a. Let's make sure the view file does not contain any inappropriate characters.
-	//    This is *vitally* important, as otherwise hackers would be able to arbitrarily
-	//    view any file on the system that your PHP process has access to, which would be bad.
-	if (preg_match('/[^\w\d._-]/', $view) !== 0)
+	// 2. Let's create a function to determine whether a view exists or not.
+	private function isValidView($view)
 	{
-		return ERROR_INVALID_FILE_NAME;
-	}
+		// 2a. Let's make sure the view file does not contain any inappropriate characters.
+		//    This is *vitally* important, as otherwise hackers would be able to arbitrarily
+		//    view any file on the system that your PHP process has access to, which would be bad.
+		if (preg_match('/[^\w\d._-]/', $view) !== 0)
+		{
+			return self::ERROR_INVALID_FILE_NAME;
+		}
 
-	// b. Let's see if the view exists.
-	$filename = "views/$view";
-	if (file_exists($filename) === false)
-	{
-		return ERROR_FILE_NOT_FOUND;
+		// 2b. Let's see if the view exists.
+		$filename = "views/$view";
+		if (file_exists($filename) === false)
+		{
+			return self::ERROR_FILE_NOT_FOUND;
+		}
+		
+		// 2c. If it got this far, it must be a valid view!
+		return self::VALID_VIEW;
 	}
-	
-	// c. If it got this far, it must be a valid view!
-	return VALID_VIEW;
 }
 
 
