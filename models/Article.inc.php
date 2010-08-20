c945a4c7b1963a58d76f7a81ee6688289f50dd86
hopeseekr <hopeseekr@xmule.ws>
2010-08-20 16:17:17 -0500

    Added the licensing terms.

--- models/Article.inc.php
+++ models/Article.inc.php
@@ -1,5 +1,21 @@
 <?php
 /**
+* Rosetta Blog
+*   Copyright © 2010 Theodore R. Smith <theodore@phpexperts.pro>
+* 
+* The following code is licensed under a modified BSD License.
+* All of the terms and conditions of the BSD License apply with one
+* exception:
+*
+* 1. Every one who has not been a registered student of the "PHPExperts
+*    From Beginner To Pro" course (http://www.phpexperts.pro/) is forbidden
+*    from modifing this code or using in an another project, either as a
+*    deritvative work or stand-alone.
+*
+* BSD License: http://www.opensource.org/licenses/bsd-license.php
+**/
+
+/**
 * The Article class is responsible for holding the data of articles and making
 * sure its properly formatted and validated, among other things.
 * It is a Struct datatype, and that is why its properties are public.
