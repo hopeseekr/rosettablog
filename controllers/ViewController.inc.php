2fb5115dcaad24301f0b9003ab328cfb9f39bdee
Theodore R. Smith <theodore@phpexperts.pro>
2012-06-12 15:08:12 -0500

    Make dates pretty.

--- controllers/ViewController.inc.php
+++ controllers/ViewController.inc.php
@@ -201,8 +201,8 @@ class ViewController
 	 */
 	private function reformatArticleProperties(&$article)
 	{
-        $article->creationDate = date('Y-m-d H:i:s', $article->creationDate);
-        $article->lastModified = date('Y-m-d H:i:s', $article->lastModified);
+        $article->creationDate = date('l, F jS, Y\<\b\r\/\> H:i:s T', $article->creationDate);
+        $article->lastModified = date('l, F jS, Y\<\b\r\/\> H:i:s T', $article->lastModified);
 
 		if ($article->format == self::ARTICLE_FORMAT_FILTERED)
 		{
